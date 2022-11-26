df52c30d7642f0301906312ef6da319b880679b0
Fazrn <87188202+Fazrn@users.noreply.github.com>
2022-11-26 19:33:18 +0330

    An input is created when a city is searched, the result will be shown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Input from "./Input";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input />
+      <p>
+        You can find the <i>open-source code</i> here.{" "}
+      </p>
     </div>
   );
 }
---
+++ src/Input.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Input() {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+  function showWeatherData(response) {
+    setWeather({ city: response.data.name, temp: response.data.main.temp });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "9feaf93d48daeaeeb2d9ea551226a8c4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeatherData);
+  }
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter a city ..."
+          type="text"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <ul>
+        <li>{weather.city}</li>
+        <li>{Math.round(weather.temp)}</li>
+      </ul>
+    </div>
+  );
+}
