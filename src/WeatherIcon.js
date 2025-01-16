5ac77865bd179d37f838a502ab687ae4bfd010c4
Fazilat Ranjbaran <fazilat.ranjbaran@pixlinemedia.de>
2025-01-16 23:39:00 +0100

    Fixed the typo for Cloudy Night

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -6,7 +6,7 @@ export default function WeatherIcon(props) {
     "01d": "CLEAR_DAY",
     "01n": "CLEAR_NIGHT",
     "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_COUDY_NIGHT",
+    "02n": "PARTLY_CLOUDY_NIGHT",
     "03d": "PARTLY_CLOUDY_DAY",
     "03n": "PARTLY_CLOUDY_NIGHT",
     "04d": "CLOUDY",
