f5c29767163eff93c706ed8bbb02ffd27300c7c2
Fazilat Ranjbaran <fazilat.ranjbaran@pixlinemedia.de>
2025-01-17 00:27:07 +0100

    Added the option to change between Fahrenheit and Celsius

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <>
-      <span className="temperature ms-2">{Math.round(props.celsius)}</span>
-      <span className="unit ms-1">°C</span>
-    </>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function convertToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  }
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <>
+        <span className="temperature ms-2">{Math.round(props.celsius)}</span>
+        <span className="unit ms-1">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <span className="temperature ms-2">
+          {Math.round(convertToFahrenheit(props.celsius))}
+        </span>
+        <span className="unit ms-1">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </>
+    );
+  }
 }
