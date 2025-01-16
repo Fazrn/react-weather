4302860ae3d3546f2ea7dfd4608964bfb55a91ab
Fazilat Ranjbaran <fazilat.ranjbaran@pixlinemedia.de>
2025-01-16 23:43:47 +0100

    Fix weather icon display and improve forecast layout in WeatherForecastDay component

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,26 +2,28 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-  console.log(props.data);
   function temperatureMax() {
     return Math.round(props.data.temp.max);
   }
+
   function temperatureMin() {
     return Math.round(props.data.temp.min);
   }
+
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return days[day];
   }
+
   return (
-    <div className="weatherForecast">
+    <div className="weatherForecastDay">
       <div className="pb-3">{day()}</div>
-      <WeatherIcon iconId={props.data.weather[0].icon} />
+      <WeatherIcon iconId={props.data.weather[0].icon} size={40} />
       <div>
-        <span>{temperatureMax()}</span>
-        <span className="ms-3 text-secondary">{temperatureMin()}</span>
+        <span>{temperatureMax()}°</span>
+        <span className="ms-3 text-secondary">{temperatureMin()}°</span>
       </div>
     </div>
   );
